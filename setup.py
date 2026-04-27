b247cf7a104b4670da8cfe014a6263b2a37d99cb
Anuj Thombare <thombareanuj2@gmail.com>
2026-04-27 18:29:20 +0530

    Loggind and Exception

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of requirements.
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if HYPEN_DOT in requirements:
+            requirements.remove(HYPEN_DOT)
+
+    return requirements
 
 setup(
-
+name='mlproject',
+version='0.0.1',
+author='Anuj',
+author_email='thombareanuj2@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 )
